a13098a5cd3be2d61851a5f5ab614285700cd9d6
Jose Ruiz <joseangel200126@hotmail.com>
2022-09-09 13:36:49 -0500

    bowling game terminado

--- inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
@@ -8,4 +8,49 @@ describe('A bowling game', () => {
     game = new BowlingGame()
   });
 
+  it('can roll a ball', () => {
+    game.roll(0)
+  })
+
+  it('can play a gutter game', () =>{
+    rollMany(game, 20, 0);
+    expect(game.score()).toBe(0)
+  })
+
+  it('can roll a game with all ones', () =>{
+    rollMany(game, 20, 1);
+    expect(game.score()).toBe(20)
+  })
+
+  it('can roll a spare', () =>{
+    rollSpare(game);
+    game.roll(1)
+    rollMany(game, 17, 0)
+    expect(game.score()).toBe(12)
+  })
+
+  it('can roll a strike', () =>{
+    game.roll(10)
+    game.roll(1)
+    game.roll(1)
+    rollMany(game, 16, 0)
+    expect(game.score()).toBe(14)
+  })
+
+  it('can roll perfect game', () => {
+    rollMany(game, 12, 10)
+    expect(game.score()).toBe(300)
+  });
 });
+
+function rollSpare(game: BowlingGame) {
+  game.roll(5);
+  game.roll(5);
+}
+
+function rollMany(game: BowlingGame, rolls:number, pins:number) {
+  for (let i = 0; i < rolls; i++) {
+    game.roll(pins);
+  }
+}
+
--- inclass/src/1_5_tdd/objectOriented/bowling.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.ts
@@ -1,5 +1,37 @@
 export class BowlingGame {
 
+    private _scoreBoard: number[]
+    constructor(){
+        this._scoreBoard = []
+    }
 
-  
+    roll(pins:number){
+        this._scoreBoard.push(pins)
+    }
+
+    score(){
+        let sum = 0
+        let firstRoll = 0
+        for (let frames = 0; frames < 10; frames++) {
+            if (this.isStrike(firstRoll)) {
+                sum += 10 + this._scoreBoard[firstRoll+1] + this._scoreBoard[firstRoll+2]
+                firstRoll++
+            }else if(this.isSpare(firstRoll)){
+                sum += 10 + this._scoreBoard[firstRoll+2]
+                firstRoll += 2
+            }else{
+                sum += this._scoreBoard[firstRoll] + this._scoreBoard[firstRoll+1]
+                firstRoll += 2
+            }   
+        }
+        return sum 
+    }
+
+    private isStrike(firstRoll:number) {
+        return this._scoreBoard[firstRoll] == 10
+    }
+
+    private isSpare(firstRoll:number) {
+        return this._scoreBoard[firstRoll] + this._scoreBoard[firstRoll+1] == 10
+    }
 }
